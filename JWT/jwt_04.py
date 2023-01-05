ca220e8bb861060b84cad0156aaf99ab813a496e
ddayanan <Dayananda.DR@netapp.com>
2023-01-05 15:56:40 +0530

    JWT - JSON Web Token with examples

--- JWT/jwt_04.py
+++ JWT/jwt_04.py
@@ -7,6 +7,7 @@ import jwt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecret"
 
+
 def verify_token(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -16,28 +17,33 @@ def verify_token(f):
             return jsonify("{'Message': 'Token is Missing'}")
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
-        except Exception as e:
-            print(e)
-            print("I am here")
+        except jwt.ExpiredSignatureError:
+            return jsonify('{"Message" : "Token got Expired"}')
+        except jwt.InvalidTokenError:
             return jsonify('{"Message":"Token is missing or invalid"}')
-        print("came out!!")
         return f(*args, **kwargs)
+
     return decorated
+
 
 @app.route("/unprotected")
 def unprotected():
     return jsonify("{'Message': 'Anyone can view this Page!!!'}")
+
 
 @app.route("/protected")
 @verify_token
 def protected():
     return jsonify('{"Message": "Token is Valid for Valid Tokens"}')
 
+
 @app.route("/login")
 def login():
     auth = request.authorization
     if auth and auth.username == "dayananda" and auth.password == "pass":
-        token = jwt.encode(payload={'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)}, key=app.config['SECRET_KEY'])
+        token = jwt.encode(
+            payload={'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5)},
+            key=app.config['SECRET_KEY'])
         return jsonify({'Token is  ': token})
     return make_response("Could not verify!", 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
