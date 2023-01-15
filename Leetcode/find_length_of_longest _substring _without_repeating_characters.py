2af230101806d0c5a987be9a7dae20d158077052
ddayanan <Dayananda.DR@netapp.com>
2023-01-15 12:11:02 +0530

    Leetcode Pratice

--- Leetcode/find_length_of_longest _substring _without_repeating_characters.py
+++ Leetcode/find_length_of_longest _substring _without_repeating_characters.py
@@ -1,9 +1,11 @@
 def find_longest_substring(string: str) -> int:
-    max_count = 1
+    max_count = 0
     for i in range(len(string)):
         count = 1
+        visited = [string[i]]
         for j in range(i + 1, len(string)):
-            if string[i] != string[j]:
+            if string[i] != string[j] and string[j] not in visited:
+                visited.append(string[j])
                 count = count + 1
                 if count > max_count:
                     max_count = count
@@ -14,4 +16,4 @@ def find_longest_substring(string: str) -> int:
     return max_count
 
 
-print(find_longest_substring("aabc"))
+print(find_longest_substring("abcabcbb"))
